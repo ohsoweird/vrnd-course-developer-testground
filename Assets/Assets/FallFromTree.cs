3b4c65ec72c21f325bcd552fd1f0c4ff5ecba523
Michael Larsson <michael@ohsoweird.com>
2016-12-17 23:27:33 +0100

    refactor: addition of scenes and 3D objects

---
+++ Assets/Assets/FallFromTree.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallFromTree : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (0, 5 + Mathf.Sin (Time.time * 5.0f), 0);
+
+	}
+}
---
+++ Assets/MyObjectMaker.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyObjectMaker : MonoBehaviour {
+
+	public GameObject objectToCreate;
+
+	// Use this for initialization
+	void Start () {
+
+		for (int i = 0; i < 50; i++) {
+		//Object.Instantiate (objectToCreate, new Vector3 (i, 4, 6), Quaternion.identity);
+			GameObject newSeagull = (GameObject)Object.Instantiate(objectToCreate, new Vector3(i, 0, 0), Quaternion.identity);
+			Renderer objectRenderer = newSeagull.GetComponentInChildren<Renderer> ();
+			objectRenderer.material.color = Color.white * Random.value;
+		}
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
